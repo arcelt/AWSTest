6dbfaef6864eeb6f4f5b2efecbf54d0cdaa9a321
gary russell <gwrussell@gmail.com>
2014-03-23 23:52:14 -0500

    yu

--- app.js
+++ app.js
@@ -29,9 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
+var ob={ info:"apple"};
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/service/')
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
